b8ec0f8a31b511cc3443304b80bb6ca2ea5aedd3
Dashadower <tttllshin@gmail.com>
2025-07-19 21:46:04 +0900

    Work on order definitions

--- theories/domains/Order.v
+++ theories/domains/Order.v
@@ -1,4 +1,5 @@
-From Coq Require Import Relations.
+From Stdlib Require Import Relations.
+From Stdlib Require Import Specif.
 (* From Coq.Relations Require Import Relation_Definitions. *)
 (* From Coq.Classes Require Import RelationClasses. *)
 
@@ -19,14 +20,43 @@ Class OrderRelation (E : Type) (R : relation E) := {
 }.
 
 Class TotalOrder (E : Type) (R : relation E) `{OrderRelation E R} := {
-  total : forall x y : E, R x y \/ R y x
+  TOtotal : forall x y : E, R x y \/ R y x
 }.
 
 Definition SubsetProp (E : Type) : Type := E -> Prop.
 Definition Subset (E : Type) (P : SubsetProp E) := {e : E | P e}.
+(* 
+A subset Subset E P represent the elements e of E that satisfies
+P e
+*)
 
-From Coq Require Import Arith.
-From Coq Require Import Lia.
+Definition SubsetRelation {E : Type} (P : SubsetProp E) (R : relation E) : relation (Subset E P) :=
+  (* Given a relation defined on E, return the same relation that is defined on Subset E P *)
+  fun (x y : Subset E P) => R (proj1_sig x) (proj1_sig y).
+
+Require Import ProofIrrelevance.
+
+Lemma relation_is_ordered_in_subset : forall (E : Type) (R : relation E) (P : SubsetProp E),
+  (* An ordered relation defined on E is also ordered on subsets of E *)
+  OrderRelation E R -> OrderRelation (Subset E P) (SubsetRelation P R).
+Proof.
+  intros.
+  constructor; inversion H.
+  - intros. inversion x. unfold SubsetRelation. apply OR_reflexive0.
+  - intros. unfold SubsetRelation in *. apply (OR_transitive0 (proj1_sig x) _ (proj1_sig z)) in H0; assumption.
+  - intros. unfold SubsetRelation in *. apply OR_antisymmetric0 in H0.
+    + destruct x. destruct y. simpl in H0. subst. f_equal. apply proof_irrelevance.
+    + assumption.
+Qed.
+
+
+Definition SubsetFunc {E F : Type} (P : SubsetProp E) (f : E -> F) : (Subset E P) -> F :=
+  (* Given a function f : E -> F, return the same function 
+     but the domain set as Subset E P  *)
+  fun (x : Subset E P) => f (proj1_sig x).
+
+From Stdlib Require Import Arith.
+From Stdlib Require Import Lia.
 Instance nat_le_is_ordered : OrderRelation nat le.
 Proof.
   constructor.
@@ -88,20 +118,62 @@ Qed.
 
 
 Class Lattice (E : Type) (R : relation E) `{OR : OrderRelation E R} := {
-  greatest_exists : exists inf, GreatestElement E R inf;
-  least_exists : exists sup, LeastElement E R sup;
-  lub_exists : forall x y, exists lub, LeastUpperBound E R OR x y lub;
-  glb_exists : forall x y, exists glb, GreatestUpperBound E R OR x y glb;
+  Lgreatest_exists : exists inf, GreatestElement E R inf;
+  Lleast_exists : exists sup, LeastElement E R sup;
+  Llub_exists : forall x y, exists lub, LeastUpperBound E R OR x y lub;
+  Lglb_exists : forall x y, exists glb, GreatestUpperBound E R OR x y glb;
 }.
 
-Check Subset nat (fun x: nat => x <= 3).
-Check Subset.
 
 Class CompleteLattice (E : Type) (R : relation E) `{Lattice E R} := {
-  subset_lub_exists : forall (P : SubsetProp E) (R' : relation (Subset E P)) (x y : Subset E P)
-                             (OR' : OrderRelation (Subset E P) R'),
-                      exists lub, LeastUpperBound (Subset E P) R' OR' x y lub;
-  subset_glb_exists : forall (P : SubsetProp E) (R' : relation (Subset E P)) (x y : Subset E P)
-                             (OR' : OrderRelation (Subset E P) R'),
-                      exists glb, GreatestUpperBound (Subset E P) R' OR' x y glb;
+  CLsubset_lub_exists : forall (P : SubsetProp E) (x y : Subset E P)
+                             (OR' : OrderRelation (Subset E P) (SubsetRelation P R)),
+                      exists lub, LeastUpperBound (Subset E P) (SubsetRelation P R) OR' x y lub;
+  CLsubset_glb_exists : forall (P : SubsetProp E) (x y : Subset E P)
+                             (OR' : OrderRelation (Subset E P) (SubsetRelation P R)),
+                      exists glb, GreatestUpperBound (Subset E P) (SubsetRelation P R) OR' x y glb;
 }.
+
+(* 
+E is a complete partial order (for short, CPO) 
+if it has an infimum and is such that any chain of elements of E has a least upper bound in E
+*)  
+Class CompletePartialOrder (E : Type) (R : relation E) (OR: OrderRelation E R) := {
+  CPOgreatest_exists : exists inf, GreatestElement E R inf;
+  CPOsubset_lub_exists : forall (P : SubsetProp E) (x y : Subset E P)
+                             (OR' : OrderRelation (Subset E P) (SubsetRelation P R)),
+                      exists lub, LeastUpperBound (Subset E P) (SubsetRelation P R) OR' x y lub;
+ }.
+
+ Definition Monotonic (A : Type) (B : Type) (f : A -> B) (R_a : relation A) (R_b : relation B)
+                     (OR_a : OrderRelation A R_a) (OR_b : OrderRelation B R_b) : Prop :=
+  forall (x y : A), R_a x y -> R_b (f x) (f y).
+
+Definition Image (A : Type) (R : relation A) (image : A) : Prop := exists a, R a image.
+
+
+(* 
+f : E -> F
+assuming that E and F are CPOs, we say that f is continuous 
+if and only if the image of any chain G of E by f has a least upper bound, 
+that is, such that ⊔{f(x) | x ∈ G} = f(⊔G).
+
+For all subset proposition P on E, if lub is a least upper bound for f()
+*)
+Definition Continuous (E F: Type) (RE : relation E) (RF : relation F) (ORE : OrderRelation E RE) (P : SubsetProp E)
+                     (ORF : OrderRelation F RF) (CPOE : CompletePartialOrder E RE ORE) (CPOF : CompletePartialOrder F RF ORF)
+                     (f : E -> F) : Prop :=
+  forall (x y : Subset E P) (OSRE : (OrderRelation (Subset E P) (SubsetRelation P RE))), 
+  exists (lub lub' : Subset E P), LeastUpperBound F RF ORF ((SubsetFunc P f) x) ((SubsetFunc P f) y) ((SubsetFunc P f) lub) ->
+                             LeastUpperBound (Subset E P) (SubsetRelation P RE) OSRE x y lub' ->
+                             (SubsetFunc P f) lub = (SubsetFunc P f) lub'.
+
+Lemma monotone_impl_continuous : forall (A B: Type) (RA : relation A) (RB : relation B) (ORA : OrderRelation A RA) (P : SubsetProp A)
+                                        (ORB : OrderRelation B RB) (E : CompletePartialOrder A RA ORA) (F : CompletePartialOrder B RB ORB)
+                                        (f : A -> B),
+  Continuous A B RA RB ORA P ORB E F f -> Monotonic A B f RA RB ORA ORB.
+Proof.
+  intros. unfold Continuous in H. unfold Monotonic. intros.
+  pose proof (relation_is_ordered_in_subset A RA P). apply H1 in ORA as ORSA.
+  destruct (H x y ORSA).
+  unfold Continuous. intros.
