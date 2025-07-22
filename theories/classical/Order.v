5f0e27e20b76b138ef2d72b04274e70b51bbf900
Dashadower <tttllshin@gmail.com>
2025-07-22 16:42:20 +0900

    Work on continuity

--- theories/classical/Order.v
+++ theories/classical/Order.v
@@ -24,21 +24,21 @@ Class TotalOrder (E : Type) (R : relation E) `{OrderRelation E R} := {
 }.
 
 Definition SubsetProp (E : Type) : Type := E -> Prop.
-Definition Subset (E : Type) (P : SubsetProp E) := {e : E | P e}.
+Definition Subset {E : Type} (P : SubsetProp E) := {e : E | P e}.
 (* 
 A subset Subset E P represent the elements e of E that satisfies
 P e
 *)
 
 (* Given a relation defined on E, return the same relation that is defined on Subset E P *)
-Definition SubsetRelation {E : Type} (P : SubsetProp E) (R : relation E) : relation (Subset E P) :=
-  fun (x y : Subset E P) => R (proj1_sig x) (proj1_sig y).
+Definition SubsetRelation {E : Type} (P : SubsetProp E) (R : relation E) : relation (Subset P) :=
+  fun (x y : Subset P) => R (proj1_sig x) (proj1_sig y).
 
 From Coq Require Import ProofIrrelevance.
 
 (* An ordered relation defined on E is also ordered on subsets of E *)
 Lemma relation_is_ordered_in_subset : forall (E : Type) (R : relation E) (P : SubsetProp E),
-  OrderRelation E R -> OrderRelation (Subset E P) (SubsetRelation P R).
+  OrderRelation E R -> OrderRelation (Subset P) (SubsetRelation P R).
 Proof.
   intros.
   constructor; inversion H.
@@ -51,8 +51,8 @@ Qed.
 
 
 (* Given a function f : E -> F, return the same function but the domain set as Subset E P  *)
-Definition SubsetFunc {E F : Type} (P : SubsetProp E) (f : E -> F) : (Subset E P) -> F :=
-  fun (x : Subset E P) => f (proj1_sig x).
+Definition SubsetFunc {E F : Type} (P : SubsetProp E) (f : E -> F) : (Subset P) -> F :=
+  fun (x : Subset P) => f (proj1_sig x).
 
 From Coq Require Import Arith.
 From Coq Require Import Lia.
@@ -80,37 +80,61 @@ Proof.
 Qed.
     
 
-Definition Chain (E : Type) (P : SubsetProp E) (R : relation (Subset E P)) `{TotalOrder (Subset E P) R} : Prop := 
-  True.
+(* Definition Chain {E : Type} (P : SubsetProp E) (R : relation E) (OR : OrderRelation E R) : Prop := 
+  forall (x y : Subset P), (SubsetRelation P R) x y \/ (SubsetRelation P R) y x.
 
-Definition GreatestElement {E : Type} (R : relation E) (x : E) :=
+Print Chain. *)
+
+Class Chain {E : Type} {R : relation E} (P : SubsetProp E) `{OrderRelation E R} : Type := {
+  Chaintotal : forall (x y : Subset P), R (proj1_sig x) (proj1_sig y) \/ R (proj1_sig y) (proj1_sig x)
+}.
+
+(* Definition is_chain {E : Type} (P : SubsetProp E) (R : relation E) (OR : OrderRelation E R) : Prop :=
+  TotalOrder (Subset P) (SubsetRelation P R). *)
+
+Definition GreatestElement {E : Type} {R : relation E} (OR : OrderRelation E R) (x : E) :=
   forall y, R y x.
 
-Definition LeastElement {E : Type} (R : relation E) (x : E) :=
+Definition LeastElement {E : Type} {R : relation E} (OR : OrderRelation E R) (x : E) :=
   forall y, R x y.
 
-Definition LeastUpperBound {E : Type} {R : relation E} (OR : OrderRelation E R) (lub : E) : Prop := 
-  forall x y, R x lub /\ R y lub /\ forall ub, R x ub -> R y ub -> R lub ub.
+Definition LeastUpperBound {E : Type} {R : relation E} (OR : OrderRelation E R) (x y lub : E) : Prop := 
+  R x lub /\ R y lub /\ forall ub, R x ub -> R y ub -> R lub ub.
 
-Definition GreatestUpperBound {E : Type} {R : relation E} (OR : OrderRelation E R) (glb : E) : Prop :=
-  forall x y, R glb x /\ R glb y /\ forall lb, R lb x -> R lb y -> R lb glb.
+Definition GreatestLowerBound {E : Type} {R : relation E} (OR : OrderRelation E R) (x y glb : E) : Prop :=
+  R glb x /\ R glb y /\ forall lb, R lb x -> R lb y -> R lb glb.
 
+Definition LeastUpperBound_Subset {E : Type} {R : relation E} (P : SubsetProp E) (OR : OrderRelation E R) (lub : E) :=
+  forall (x : Subset P), (R (proj1_sig x) lub /\ forall ub, R (proj1_sig x) ub -> R lub ub).
 
-Theorem LeastUpperBound_unique : forall {E : Type} {R : relation E} (OR : OrderRelation E R) (lub lub' : E),
-  LeastUpperBound OR lub -> LeastUpperBound OR lub' -> lub = lub'.
+Definition GreatestLowerBound_Subset {E : Type} {R : relation E} (P : SubsetProp E) (OR : OrderRelation E R) (glb : E) :=
+  forall (x : Subset P), (R glb (proj1_sig x) /\ forall lb, R lb (proj1_sig x) -> R lb glb).
+
+Theorem LeastUpperBound_unique : forall {E : Type} {R : relation E} (OR : OrderRelation E R) (x y lub lub' : E),
+  LeastUpperBound OR x y lub -> LeastUpperBound OR x y lub' -> lub = lub'.
 Proof.
-  intros. unfold LeastUpperBound in *. destruct (H lub lub') as [H1 [H2 H3]].
-  destruct (H0 lub lub') as [H4 [H5 H6]]. inversion OR. apply OR_antisymmetric in H4.
-  - symmetry. assumption.
+  intros. unfold LeastUpperBound in *. destruct H as [H1 [H2 H3]].
+  destruct H0 as [H4 [H5 H6]]. inversion OR.
+  apply H3 in H4.
+  - apply H6 in H1.
+    + apply OR_antisymmetric in H4.
+      * symmetry. assumption.
+      * assumption.
+    + assumption.
   - assumption.
 Qed.
 
-Theorem GreatestUpperBound_unique : forall {E : Type} {R : relation E} (OR : OrderRelation E R) (glb glb' : E),
-  GreatestUpperBound OR glb -> GreatestUpperBound OR glb' -> glb = glb'.
+Theorem GreatestLowerBound_unique : forall {E : Type} {R : relation E} (OR : OrderRelation E R) (x y glb glb' : E),
+  GreatestLowerBound OR x y glb -> GreatestLowerBound OR x y glb' -> glb = glb'.
 Proof.
-  intros. unfold GreatestUpperBound in *. destruct (H glb glb') as [H1 [H2 H3]].
-  destruct (H0 glb glb') as [H4 [H5 H6]]. apply OR_antisymmetric in H4.
-  - assumption.
+  intros. unfold GreatestLowerBound in *. destruct H as [H1 [H2 H3]].
+  destruct H0 as [H4 [H5 H6]].
+  apply H3 in H4.
+  - apply H6 in H1.
+    + apply OR_antisymmetric in H1.
+      * symmetry. assumption.
+      * assumption.
+    + assumption.
   - assumption.
 Qed.
 
@@ -118,36 +142,40 @@ Qed.
 Class Lattice {E : Type} (R : relation E) `{OR : OrderRelation E R} := {
   Lattice_top : E;
   Lattice_bottom : E;
-  Lgreatest_exists : GreatestElement R Lattice_top;
-  Lleast_exists : LeastElement R Lattice_bottom;
-  Llub_exists : exists lub, LeastUpperBound OR lub;
-  Lglb_exists : exists glb, GreatestUpperBound OR glb;
+  Lgreatest_exists : GreatestElement OR Lattice_top;
+  Lleast_exists : LeastElement OR Lattice_bottom;
+  Llub_exists : forall x y, exists lub, LeastUpperBound OR x y lub;
+  Lglb_exists : forall x y, exists glb, GreatestLowerBound OR x y glb;
 }.
 
+Print Lattice.
 
-Class CompleteLattice (E : Type) (R : relation E) `{Lattice E R} := {
-  CLsubset_lub_exists : forall (P : SubsetProp E) (x y : Subset E P)
-                             (OR' : OrderRelation (Subset E P) (SubsetRelation P R)),
-                      exists lub, LeastUpperBound OR' lub;
-  CLsubset_glb_exists : forall (P : SubsetProp E) (x y : Subset E P)
-                             (OR' : OrderRelation (Subset E P) (SubsetRelation P R)),
-                      exists glb, GreatestUpperBound OR' glb;
+(* Class CompleteLattice (E : Type) (R : relation E) `{L : Lattice (relation E)} := {
+  CL : E
+}.
+
+Print CompleteLattice. *)
+
+Class CompleteLattice {E : Type} (R : relation E) `{L : Lattice E} := {
+  CLsubset_lub_exists : forall (P : SubsetProp E),
+                      exists lub : E, LeastUpperBound_Subset P OR lub;
+  CLsubset_glb_exists : forall (P : SubsetProp E),
+                      exists glb : E, GreatestLowerBound_Subset P OR glb;
 }.
 
 (* 
 E is a complete partial order (for short, CPO) 
-if it has an infimum and is such that any chain of elements of E has a least upper bound in E
+if it has an infimum(bottom) and is such that any chain of elements of E has a least upper bound in E
 *)  
 Class CompletePartialOrder {E : Type} {R : relation E} (OR: OrderRelation E R) := {
   CPO_bottom : E;
-  CPOleast_exists : LeastElement R CPO_bottom;
-  CPOsubset_lub_exists : forall (P : SubsetProp E) (x y : Subset E P)
-                             (OR' : OrderRelation (Subset E P) (SubsetRelation P R)),
-                      exists lub, LeastUpperBound OR' lub;
+  CPOleast_exists : LeastElement OR CPO_bottom;
+  CPOsubset_lub_exists : forall (P : SubsetProp E) (G : Chain P),
+                      exists lub, LeastUpperBound_Subset P OR lub;
  }.
 
  Definition Monotonic {E : Type} {F : Type} {RE : relation E} {RF : relation F}
-                     (ORE : OrderRelation E RE) (ORF : OrderRelation F RF) (f : E -> F) : Prop :=
+                      (ORE : OrderRelation E RE) (ORF : OrderRelation F RF) (f : E -> F) : Prop :=
   forall (x y : E), RE x y -> RF (f x) (f y).
 
 Definition Image (A : Type) (R : relation A) (image : A) : Prop := exists a, R a image.
@@ -160,29 +188,41 @@ assuming that E and F are CPOs, we say that f is continuous
 if and only if the image of any chain G of E by f has a least upper bound, 
 that is, such that ⊔{f(x) | x ∈ G} = f(⊔G).
 *)
-Definition LeastUpperBound_proj_f {E F : Type} {R : relation F} (OR : OrderRelation F R) (f : E -> F) (lub : E) : Prop := 
-  forall x y, R (f x) (f lub) /\ R (f y) (f lub) /\ forall ub, R (f x) (f ub) -> R (f y) (f ub) -> R (f lub) (f ub).
+
+(* 
+f : E -> E
+R : relation E (E -> E -> Prop)
+R_f : R (f x) (f x)
+*)
+
+Definition LeastUpperBound_Subset_lift_f {E F : Type} {R : relation F} (P : SubsetProp E) (ORF : OrderRelation F R) (f: E -> F) (lub : F) :=
+  forall (x : Subset P), (R (f (proj1_sig x)) lub /\ forall ub, R (f(proj1_sig x)) ub -> R lub ub).
 
 Definition Continuous {E F: Type} {RE : relation E} {RF : relation F} {ORE : OrderRelation E RE}
                      {ORF : OrderRelation F RF} (CPOE : CompletePartialOrder ORE) (CPOF : CompletePartialOrder ORF)
                      (f : E -> F) : Prop :=
-  forall (P : SubsetProp E) (G : (OrderRelation (Subset E P) (SubsetRelation P RE))), 
-  exists (lub lub' : Subset E P), LeastUpperBound_proj_f ORF (SubsetFunc P f) lub ->
-                             LeastUpperBound G lub' ->
-                             f (proj1_sig lub) = f (proj1_sig lub').
+  forall (P : SubsetProp E) (G : Chain P),
+  exists (lub : F) (lub' : E), LeastUpperBound_Subset_lift_f P ORF f lub /\
+                            LeastUpperBound_Subset P ORE lub' ->
+                            lub = f lub'.
 
 
 (* A continuous function is also monotone *)
 Lemma continuous_impl_monotone : forall (E F: Type) (RE : relation E) (RF : relation F) (ORE : OrderRelation E RE) (P : SubsetProp E)
+                                        (G : Chain P)
                                         (ORF : OrderRelation F RF) (CPOE : CompletePartialOrder ORE) (CPOF : CompletePartialOrder ORF)
                                         (f : E -> F),
   Continuous CPOE CPOF f -> Monotonic ORE ORF f.
 Proof.
-  intros. unfold Continuous in H. 
-  unfold Monotonic.
-  pose proof (relation_is_ordered_in_subset E RE P). apply H0 in ORE as ORSE.
-  destruct (H P ORSE) as[lub [lub' H1]]. clear H H0.
-  intros.
+  intros. 
+  pose proof Chaintotal.
+  unfold Monotonic. intros.
+  pose proof (CPOsubset_lub_exists P G) as lub_e. destruct lub_e as [lub_e Hlub]. unfold LeastUpperBound_Subset in Hlub.
+  
+  unfold Continuous in H. 
+  unfold Monotonic. intros.
+  destruct (H P) as [lub_fe [lub_e' [H1 [H2 H3]]]];[assumption|].
+  clear H. unfold LeastUpperBound_Subset_lift_f in H1.
 Abort.
 
 
